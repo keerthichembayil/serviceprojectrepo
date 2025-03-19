82f5688321170bc36c92ea402e8e3917b6d6f4ee
keerthichembayil <keerthichembayil@gmail.com>
2025-03-19 06:45:59 +0530

    addedfav

--- server/server.js
+++ server/server.js
@@ -21,7 +21,8 @@ const reviewRoutes=require('./routes/reviewroute');
 
 
 const app = express();
-
+// OR for PNG
+app.use('/favicon.png', express.static(path.join(__dirname, 'public', 'favicon.png')));
 
 app.use(cors({
   // origin: process.env.FRONTEND_URL || "http://localhost:5173",
@@ -55,3 +56,4 @@ app.use("/api/review",reviewRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port http://localhost: ${PORT}`));
+module.exports = app; 
