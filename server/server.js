08e737d6e1d47efdf41b324db02684774f3fc995
keerthichembayil <keerthichembayil@gmail.com>
2025-03-19 07:14:40 +0530

    appexpo

--- server/server.js
+++ server/server.js
@@ -60,4 +60,4 @@ app.use("/api/review",reviewRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port http://localhost: ${PORT}`));
-module.exports = app; 
+// module.exports = app; 
