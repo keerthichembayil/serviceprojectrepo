76fb8388c06dffeb2627ba8303859d1ef43dff77
keerthichembayil <keerthichembayil@gmail.com>
2025-03-20 15:06:30 +0530

    changedser

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ app.use(bodyParser.json());
 
 
 mongoose
-  .connect(process.env.MONGO_URI//, { useNewUrlParser: true, useUnifiedTopology: true }//
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }
     )
   .then(() => console.log('Connected to MongoDB atlas'))
   .catch((err) => console.error('MongoDB connection error:', err));
@@ -62,4 +62,4 @@ app.use("/api/review",reviewRoutes);
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port http://localhost: ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
